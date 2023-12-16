60568331653b8b9d494637367feb97667656c6d0
Reja <rezanurrochmat@gmail.com>
2023-12-16 15:37:24 +0700

    bootstrap todo crud endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'anakkucing',
+      password: 'anakkucing',
+      entities: [],
+      database: 'todolist',
+      synchronize: true,
+      logging: true,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
